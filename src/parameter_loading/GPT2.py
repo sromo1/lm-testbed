6f5240721745d35e6e5cb258057dcd807b62df1e
sromo1 <sromoac+github@gmail.com>
2026-02-19 08:58:55 -0500

    Refactor GPT model structure and update import paths
    - Changed project root directory from `lm-testbed` to `lm-workbench`.
    - Updated import paths for `DummyGPTModel`, `TransformerBlock`, and `GPTModel` to reflect new architecture organization.
    - Removed deprecated `gpt_download.py` script and added a new version in `download_parameters` directory.
    - Deleted old model files in `src/models` and moved model definitions to `src/architectures`.
    - Introduced new `SpamDataset` class for loading and processing spam data.
    - Implemented weight loading functions in `src/parameter_loading` with helper functions for tensor assignment.

---
+++ src/parameter_loading/GPT2.py
@@ -0,0 +1,80 @@
+import numpy as np
+import torch.nn as nn
+from .helpers import assign
+
+def load_weights(gpt:nn.Module, params:dict):
+    gpt.pos_emb.weight = assign(gpt.pos_emb.weight, params['wpe'])
+    gpt.tok_emb.weight = assign(gpt.tok_emb.weight, params['wte'])
+
+    for b in range(len(params["blocks"])):
+        # Attention: Weight Matrices
+        q_w, k_w, v_w = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["w"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.weight = assign(gpt.trf_blocks[b].att.W_query.weight, q_w.T)
+        gpt.trf_blocks[b].att.W_key.weight = assign(gpt.trf_blocks[b].att.W_key.weight, k_w.T)
+        gpt.trf_blocks[b].att.W_value.weight = assign(gpt.trf_blocks[b].att.W_value.weight, v_w.T)
+
+        # Attention: Weight Matrices Bias Vectors
+        q_b, k_b, v_b = np.split(
+            (params["blocks"][b]["attn"]["c_attn"])["b"], 3, axis=-1)
+        gpt.trf_blocks[b].att.W_query.bias = assign(gpt.trf_blocks[b].att.W_query.bias, q_b)
+        gpt.trf_blocks[b].att.W_key.bias = assign(gpt.trf_blocks[b].att.W_key.bias, k_b)
+        gpt.trf_blocks[b].att.W_value.bias = assign(gpt.trf_blocks[b].att.W_value.bias, v_b)
+
+        # Attention: Output Projection Matrix
+        gpt.trf_blocks[b].att.out_proj.weight = assign(
+            gpt.trf_blocks[b].att.out_proj.weight,
+            params["blocks"][b]["attn"]["c_proj"]["w"].T
+            )
+        
+        # Attention: Output Projection Matrix Bias
+        gpt.trf_blocks[b].att.out_proj.bias = assign(
+            gpt.trf_blocks[b].att.out_proj.bias,
+            params["blocks"][b]["attn"]["c_proj"]["b"]
+            )
+        
+        # Feed Forward Blocks - Layer 1: Weights and Biases
+        gpt.trf_blocks[b].ff.layers[0].weight = assign(
+            gpt.trf_blocks[b].ff.layers[0].weight,
+            params["blocks"][b]["mlp"]["c_fc"]["w"].T
+            )
+        gpt.trf_blocks[b].ff.layers[0].bias = assign(
+            gpt.trf_blocks[b].ff.layers[0].bias,
+            params["blocks"][b]["mlp"]["c_fc"]["b"]
+            )
+        # Feed Forward Blocks - Layer 2: Weights and Biases
+        gpt.trf_blocks[b].ff.layers[2].weight = assign(
+            gpt.trf_blocks[b].ff.layers[2].weight,
+            params["blocks"][b]["mlp"]["c_proj"]["w"].T
+            )
+        gpt.trf_blocks[b].ff.layers[2].bias = assign(
+            gpt.trf_blocks[b].ff.layers[2].bias,
+            params["blocks"][b]["mlp"]["c_proj"]["b"]
+            )
+        
+        # Pre-Norm 1 - Attention
+        gpt.trf_blocks[b].norm1.scale = assign(
+            gpt.trf_blocks[b].norm1.scale,
+            params["blocks"][b]["ln_1"]["g"]
+            )
+        gpt.trf_blocks[b].norm1.shift = assign(
+            gpt.trf_blocks[b].norm1.shift,
+            params["blocks"][b]["ln_1"]["b"]
+            )
+        
+        # Pre-Norm 2 - FF
+        gpt.trf_blocks[b].norm2.scale = assign(
+            gpt.trf_blocks[b].norm2.scale,
+            params["blocks"][b]["ln_2"]["g"]
+            )
+        gpt.trf_blocks[b].norm2.shift = assign(
+            gpt.trf_blocks[b].norm2.shift,
+            params["blocks"][b]["ln_2"]["b"]
+            )
+    
+    # Final Norm
+    gpt.final_norm.scale = assign(gpt.final_norm.scale, params["g"])
+    gpt.final_norm.shift = assign(gpt.final_norm.shift, params["b"])
+
+    # Linear Output Layer - OpenAI's GPT2 reused the token embedding weights in the output layer
+    gpt.out_head.weight = assign(gpt.out_head.weight, params["wte"])
